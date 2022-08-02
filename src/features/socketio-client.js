29b2cbea6ff3975ee22a374a8287b0f787c5ec80
JustinChen <yub612@gmail.com>
2022-08-02 22:01:04 +0800

    fix bug(stupid typo) start writing socket

--- src/features/socketio-client.js
+++ src/features/socketio-client.js
@@ -3,6 +3,9 @@ const socket = io("http://localhost:8080",{
     path: "/chat",
     transports: ['websocket']
 });
+socket.on("onnection",async () => {
+    console.log("jizz");
+});
 
 // const receiveMsg = ws.on(async() => {
 //     await ws.emit("");
